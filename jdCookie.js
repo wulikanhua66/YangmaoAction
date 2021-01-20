f1623bd65fbaca2440ff21dc2a6749a9706e4b23
banna <banna.guo@tuya.com>
2021-01-20 17:37:31 +0800

    add cookie

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,7 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJgBRXfADBRyeVBAlpyDPKNfV-ZUJKBc7JhQajTINFR7RBhRIlqoNTHWLirvAwkUw4mui74CCo;pt_pin=545323722;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断环境变量里面是否有京东ck
